8b755feb9b31dac25ee896126ba6102555702118
artakbabayan <babayanartak12@gmail.com>
2020-08-04 14:42:35 +0400

    added Info component

---
+++ src/components/info/Info.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import './style.css';
+import Card from '@material-ui/core/Card';
+
+
+function Info() {
+
+
+  return (
+
+    <Card className="info">
+      <h3>Graduate Computer Science @ Showwcase University</h3>
+      <div>
+        <p>August 2019 - Present</p>
+        <li>"Not all those who wander are lost"</li>
+        <li>"Not all those who wander are lost"</li>
+        <li>"Not all those who wander are lost"</li>
+      </div>
+    </Card>
+
+  );
+}
+
+export default Info;
